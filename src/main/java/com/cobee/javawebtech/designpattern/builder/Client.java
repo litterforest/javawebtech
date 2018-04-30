54019349b7bec1c755ccd592f53863e374f4d314
gensen <18027041863@163.com>
2018-04-30 08:54:04 +0800

    测试使用构建器设置属性和硬编码使用属性的性能效率。

--- src/main/java/com/cobee/javawebtech/designpattern/builder/Client.java
+++ src/main/java/com/cobee/javawebtech/designpattern/builder/Client.java
@@ -9,9 +9,39 @@ import java.lang.reflect.InvocationTargetException;
 public class Client {
 
     public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
-        Person person = new Person();
-        person.setProperty("name", "cobee").setProperty("age", 30);
-        System.out.println(person);
+//        test1();
+        test2();
+    }
+
+    public static final void test1() throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        long start = System.currentTimeMillis();
+        Person person = null;
+        for (int i = 0; i < 100000; i++)
+        {
+            person = new Person();
+            person.setProperty("name", "cobee").setProperty("age", 30);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println((end - start) + "毫秒");
+    }
+
+    public static final void test2() throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        long start = System.currentTimeMillis();
+        Runtime runtime = Runtime.getRuntime();
+        long freeMenory = runtime.freeMemory();
+        System.out.println("start: " + freeMenory);
+//        Person person = null;
+        for (int i = 0; i < 10000000; i++)
+        {
+            Person person = new Person();
+            person.setAge(30);
+            person.setName("cobee");
+        }
+        long end = System.currentTimeMillis();
+        System.out.println((end - start) + "毫秒");
+        long freeMenory1 = runtime.freeMemory();
+        System.out.println("end: " + freeMenory1);
+        System.out.println("使用了多少字节内存：" + ((freeMenory - freeMenory1) / 1024 / 1024) + "MB");
     }
 
 }
